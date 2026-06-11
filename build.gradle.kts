97b2e90a6b600b10d2c9be83a4e58c5565487ba9
Evgeny Bokshitsky <e.bokshitskiy@hh.ru>
2026-06-12 03:15:34 +0400

    build: scaffold IntelliJ plugin project with PythonCore dependency

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "2.0.21"
+    id("org.jetbrains.intellij.platform") version "2.5.0"
+}
+
+group = "com.boksh"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        intellijIdeaCommunity("2024.3.5")
+        plugin("PythonCore", "243.24978.46")
+    }
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation(kotlin("stdlib"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "242"
+        }
+    }
+    buildSearchableOptions = false
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
